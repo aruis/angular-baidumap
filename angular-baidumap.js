06faed4764262605e1fce74467c04d381394932e
aruis <lovearuis@gmail.com>
2016-05-08 07:21:18 +0800

    add bmap-marker

--- angular-baidumap.js
+++ angular-baidumap.js
@@ -7,10 +7,13 @@ angular.module('angular-baidumap-aruis', [])
             restrict: 'E',
             template: '<div style="width:100%;height:100%;"></div>',
             replace: true,
+            scope: {
+                bmapMarker: '='
+            },
             link: function (scope, element, attrs) {
                 $timeout(function () {
                     var map = new BMap.Map(element[0]);    // 创建Map实例
-                    var city = attrs.bmapCity == null ? '徐州' : attrs.bmapCity;
+                    var city = attrs.bmapCity == null ? new BMap.Point(116.404, 39.915) : attrs.bmapCity;
                     var zoom = attrs.bmapZoom == null ? '10' : attrs.bmapZoom;
 
                     map.centerAndZoom(city, zoom);  // 初始化地图,设置中心点坐标和地图级别
@@ -31,9 +34,31 @@ angular.module('angular-baidumap-aruis', [])
                         map.addControl(new BMap.OverviewMapControl());
                     }
 
+                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+                    var marker;
+                    scope.$watch('bmapMarker', function (_new) {
+                        if (_new == undefined || _new == null) {
+                            if (marker) {
+                                map.removeOverlay(marker);
+                            }
+                            return;
+                        }
+
+                        var point = new BMap.Point(_new.lng, _new.lat);
+                        map.centerAndZoom(point, _new.zoom > 0 ? _new.zoom : 16)
+
+                        if (marker) {
+                            marker.setPosition(point)
+                        } else {
+                            marker = new BMap.Marker(point);        // 创建标注
+                        }
+                        map.addOverlay(marker);
+                    }, true)
+
 
                     //map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
                 });
 
             }
--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,14 @@
 /**
  * Created by liurui on 15/11/21.
  */
-angular.module('demo',['angular-baidumap-aruis']);
+angular.module('demo', ['angular-baidumap-aruis'])
+    .controller('mainCtrl', function ($scope, $timeout) {
+
+
+        $scope.point = {
+            lng: 116.387112,
+            lat: 39.9209777,
+            zoom: 16
+        }
+
+    })
